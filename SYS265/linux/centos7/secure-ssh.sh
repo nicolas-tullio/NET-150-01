c56772b2947625df776a2a997623059567a8ee4c
nicolas-tullio <nicolas.tullio@mymail.champlain.edu>
2022-10-20 00:04:17 -0400

    add user

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,11 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+echo Create user name?
+read USERNAME
+sudo useradd -m -d /home/$USERNAME -s /bin/bash $USERNAME
+sudo mkdir /home/$USERNAME/.ssh
+sudo cp /home/nicolas.tullio/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$USERNAME/.ssh/authorized_keys
+sudo chmod 700 /home/$USERNAME/.ssh
+sudo chmod 600 /home/$USERNAME/.ssh/authorized_keys
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
